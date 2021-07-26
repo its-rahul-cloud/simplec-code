f0f92564e74679cf738449c9f3006042a1f739f6
its-rahul-cloud <71515596+its-rahul-cloud@users.noreply.github.com>
2021-07-27 01:44:49 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+
+using namespace std;
+int fibo(int n)
+{
+
+    int a=0;
+    int b=1;
+    int c;
+ if(n>3){
+    for(int i=2;i<=n-1;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+
+    }
+    return c;
+ }
+ else{
+     
+     if(n==2 || n==3)
+     return 1;
+     
+     else
+
+     return 0;
+     }
+}
+
+ int main()
+ {
+     int n;
+     cin>>n;
+     cout<<fibo(n);
+     return 0;
+ }
